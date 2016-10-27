ee94943a25442a5e895c883406e5346aac0f1d11
Pratyasa Mandal <pratyasam@mindfiresolutions.com>
2016-10-27 10:47:57 +0530

    updated exception controller class

--- src/main/java/com/mindfire/review/web/controllers/ExceptionController.java
+++ src/main/java/com/mindfire/review/web/controllers/ExceptionController.java
@@ -4,9 +4,12 @@
 package com.mindfire.review.web.controllers;
 
 import java.io.FileNotFoundException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.fileupload.FileUploadException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.apache.commons.fileupload.FileUploadException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -24,13 +27,17 @@ public class ExceptionController {
 
 	public static final String ERRORINTERNAL = "internalerror";
 	public static final String DEFAULT_ERROR_VIEW = "resourcenotfound";
+	
+	private static final Logger logger = LoggerFactory.getLogger(ExceptionController.class);
 
 	/**
 	 * 
 	 * @return
 	 */
 	@ExceptionHandler({RuntimeException.class, FileUploadException.class})
-	public String internalError() {
+	public String internalError(HttpServletRequest request, Exception ex) {
+		
+		logger.info("Internal Occured:: URL="+request.getRequestURL());
 		return ERRORINTERNAL;
 	}
 	
@@ -40,8 +47,9 @@ public class ExceptionController {
 	 */
 	@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="User Not Found")
 	@ExceptionHandler({FileNotFoundException.class,BookDoesNotExistException.class, UserDoesNotExistException.class})
-	public String defaultErrorHandler() {
+	public String defaultErrorHandler(HttpServletRequest request, Exception ex) {
 		
+		logger.error("Resource not found");
 		// Send the user to a default error-view.
 		return DEFAULT_ERROR_VIEW;
 	}
